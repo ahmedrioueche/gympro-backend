215e3256d9380c2eb22c24c729ac059edc70ab87
Ahmed Drioueche <adsrahmed@gmail.com>
2025-08-17 22:17:19 +0100

    Remove dist folder from tracking

--- dist/modules/users/services/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Db } from 'mongodb';
-export declare class UsersService {
-    private readonly db;
-    constructor(db: Db);
-    findAll(): Promise<import("mongodb").WithId<import("bson").Document>[]>;
-    create(user: any): Promise<import("mongodb").InsertOneResult<import("bson").Document>>;
-}
